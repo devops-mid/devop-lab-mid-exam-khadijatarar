4a9d37199a4b744cb9e325ba549e99a0285e7109
khadijatarar <147639487+khadijatarar@users.noreply.github.com>
2025-03-19 10:14:23 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Building the application..."
-# TODO: Add commands to install dependencies and build the app
+
+# Stop if there's an error
+set -e
+
+# Build the Docker image
+echo "Building Docker image..."
+docker build -t my-app .
+
+echo "Build complete!"
